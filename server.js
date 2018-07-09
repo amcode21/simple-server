caef626f994eb0faede474d4f318d1e3f50e5793
Arnav Malhotra <spideynuff@gmail.com>
2018-07-09 14:22:37 -0700

    Initial Project Commit

---
+++ server.js
@@ -0,0 +1,24 @@
+require('console-stamp')(console, {
+    metadata: function () {
+        return ('[' + process.memoryUsage().rss + ']');
+    },
+    colors: {
+        stamp: 'yellow',
+        label: 'white',
+        metadata: 'green'
+    }
+});
+
+let express = require('express');
+let http = require('http');
+
+let app = express();
+app.server = http.createServer(app);
+
+app.get('/', (req, res) => {
+	res.send('Hello, World!');
+});
+
+app.server.listen(process.env.PORT || 8080, () => {
+	console.info('Listening @ http://127.0.0.1:' + app.server.address().port);
+});
